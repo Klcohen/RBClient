05fb3b25e6746b0093de932135e7f87a76484260
Danielle <dlgeiser2992@gmail.com>
2019-07-11 09:41:00 -0400

    components skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SigninComponent } from './signin/signin.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { FeedCardComponent } from './feed-card/feed-card.component';
+import { CommentsComponent } from './comments/comments.component';
+import { ProfileComponent } from './profile/profile.component';
+import { JournalComponent } from './journal/journal.component';
+import { ArtCreateComponent } from './art-create/art-create.component';
+import { PostButtonComponent } from './post-button/post-button.component';
+import { InspireButtonComponent } from './inspire-button/inspire-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent,
+    NavbarComponent,
+    SearchBarComponent,
+    FeedCardComponent,
+    CommentsComponent,
+    ProfileComponent,
+    JournalComponent,
+    ArtCreateComponent,
+    PostButtonComponent,
+    InspireButtonComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/inspire-button/inspire-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inspire-button',
+  templateUrl: './inspire-button.component.html',
+  styleUrls: ['./inspire-button.component.css']
+})
+export class InspireButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
