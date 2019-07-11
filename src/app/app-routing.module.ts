c5bf193cc1408f1bcba267ffbbae2195c85ccb75
Danielle <dlgeiser2992@gmail.com>
2019-07-11 14:26:33 -0400

    routing start

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ArtCreateComponent } from './art-create/art-create.component'
+import { FeedComponent } from './feed/feed.component'
+import { ProfileComponent } from './profile/profile.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: ArtCreateComponent},
+  { path: 'feed', component: FeedComponent},
+  {path: 'profile', component: ProfileComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
