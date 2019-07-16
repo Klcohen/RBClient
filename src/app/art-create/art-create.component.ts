db27d75af6c1b0b4ff52b152c91f671e7f51e332
Danielle <dlgeiser2992@gmail.com>
2019-07-16 09:21:02 -0400

    grid create working

--- src/app/art-create/art-create.component.ts
+++ src/app/art-create/art-create.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { FormGroup, FormControl } from '@angular/forms';
 import { RouterLinkWithHref } from '@angular/router';
+import { NgForOf } from '@angular/common';
 
 @Component({
   selector: 'app-art-create',
@@ -12,24 +13,42 @@ export class ArtCreateComponent implements OnInit {
   sizePicker = new FormGroup({
     height: new FormControl(''),
     width: new FormControl(''),
-    });
+  });
+  canvas: any[];
 
   constructor() { }
 
   onSubmit() {
 
     console.warn(this.sizePicker.value);
-    return this.sizePicker.value;
 
-    // canvas = $('#pixel_canvas');
-	  let gridHeight = this.sizePicker.value.height;
-	  let gridWidth = this.sizePicker.value.width;
 
-    // ngfor row for each within each row make column
-    
+    let gridHeight = this.sizePicker.value.height;
+    let gridWidth = this.sizePicker.value.width;
+
+    //****************************************** 
+    // USE ARRAY METHODS TO CLEAN UP AND SHORTEN
+    // *****************************************
+    let canvas = [] 
+    for (let row = 0; row < gridHeight; row++) {
+      for (let column = 0; column < gridWidth; column++) {
+        if (canvas[row]) {
+          canvas[row][column] = undefined
+        } else {
+          canvas[row] = [undefined]
+        }
+      }
+    }
+
+    console.log(canvas)
+    this.canvas = canvas
+
+
+
   }
 
   ngOnInit() {
+    
   }
 
 }
